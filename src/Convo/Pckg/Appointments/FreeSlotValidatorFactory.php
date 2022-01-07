07e81432cab50300bcd9588bf9c02731b964328d
Tihomir Dmitrovic <tole.car@gmail.com>
2022-01-07 23:53:22 +0100

    Default queue

--- src/Convo/Pckg/Appointments/FreeSlotValidatorFactory.php
+++ src/Convo/Pckg/Appointments/FreeSlotValidatorFactory.php
@@ -6,6 +6,7 @@ namespace Convo\Pckg\Appointments;
 
 class FreeSlotValidatorFactory
 {
+    const KEY_MAX_PER_DAY           =   'max_per_day';
     const KEY_FIRST_NEXT            =   'first_next';
     const KEY_FIRST_SAME_TIME       =   'first_same_time';
     const KEY_FIRST_SAME_DAY_TIME   =   'first_same_day_and_time';
@@ -27,6 +28,7 @@ class FreeSlotValidatorFactory
     {
         $MAX      =   3;
         $queue    =   new FreeSlotQueue( $MAX, []);
+        $queue->addValidator( $this->create( FreeSlotValidatorFactory::KEY_MAX_PER_DAY));
         $queue->addValidator( $this->create( FreeSlotValidatorFactory::KEY_FIRST_NEXT));
         $queue->addValidator( $this->create( FreeSlotValidatorFactory::KEY_FIRST_SAME_TIME));
         $queue->addValidator( $this->create( FreeSlotValidatorFactory::KEY_FIRST_SAME_DAY_TIME));
@@ -36,6 +38,22 @@ class FreeSlotValidatorFactory
     
     public function create( $key)
     {
+        if ( $key == self::KEY_MAX_PER_DAY) {
+            return new class( $this->_time) extends DefaultFreeSlotValidator {
+                
+                private $_days  =   [];
+                
+                public function add( $item)
+                {
+                    $slot_date  =   \DateTime::createFromFormat( 'U', strval( $item['timestamp']));
+                    if ( $this->_time->format( 'W') < $slot_date->format( 'W')) {
+                        return parent::add( $item);
+                    }
+                    return false;
+                }
+            };
+        }
+        
         if ( $key == self::KEY_FIRST_NEXT) {
             return new DefaultFreeSlotValidator( $this->_time);
         }
@@ -84,6 +102,8 @@ class FreeSlotValidatorFactory
                 }
             };
         }
+        
+        throw new \Exception( 'Unexpected key ['.$key.']');
     }
     
     // UTIL
