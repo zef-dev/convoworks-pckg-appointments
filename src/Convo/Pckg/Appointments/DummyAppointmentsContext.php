3fcb7509399bfc372f73d5e69194903845460968
mmarcec007 <mark.marcec@gmail.com>
2022-01-24 14:29:46 +0100

    fix DummyAppointmentsContext

--- src/Convo/Pckg/Appointments/DummyAppointmentsContext.php
+++ src/Convo/Pckg/Appointments/DummyAppointmentsContext.php
@@ -19,9 +19,9 @@ class DummyAppointmentsContext extends AbstractBasicComponent implements IServic
 	const MAX_HOUR         =   '16:30';
 	const DURATION_MINUTES =   30;
 	const MAX_DAYS         =   15;
-	
+
 	private $_cachedAppointments;
-	
+
 	public function __construct( $properties)
 	{
 		parent::__construct( $properties);
@@ -49,54 +49,68 @@ class DummyAppointmentsContext extends AbstractBasicComponent implements IServic
 	{
 		return $this->_id;
 	}
-	
+
 	// APPOINTMENTS INTERFACE
 	public function isSlotAvailable( $time)
 	{
 	    $this->_logger->debug( 'Checking time ['.$time->format( self::DATE_TIME_FORMAT).']');
-	    
-	    if ( !$this->_checkSlotAllowed( $time)) {
-	        return false;
-	    }
-	    
+
+	    $this->_checkSlotAllowed( $time);
+
 	    $appointments  =   $this->_getAppointments();
-	    
-	    foreach ( $appointments as $appointment) 
+
+	    foreach ( $appointments as $appointment)
 	    {
 	        $start     =   \DateTime::createFromFormat( 'U', strval( $appointment['timestamp']), $time->getTimezone());
 	        $end       =   \DateTime::createFromFormat( 'U', strval( $appointment['timestamp'] + self::DURATION_MINUTES * 60), $time->getTimezone());
-	        
+
 	        $this->_logger->debug( 'Checking against ['.$start->format( self::DATE_TIME_FORMAT).']['.$end->format( self::TIME_FORMAT).']');
 	        if ( $time >= $start && $time < $end) {
 	            $this->_logger->info( 'Taken slot by ['.$appointment['appointment_id'].']');
 	            return false;
 	        }
 	    }
-	    
+
 	    return true;
 	}
-	
+
 	/**
-	 * @param \DateTime $time
+	 * @param \DateTimeInterface $time
 	 * @throws OutOfBusinessHoursException
 	 */
-	private function _checkSlotAllowed( $time) 
+	private function _checkSlotAllowed( $time)
 	{
 	    $requested =   \DateTime::createFromFormat( 'H:i', $time->format( 'H:i'));
 	    $start     =   \DateTime::createFromFormat( 'H:i', self::MIN_HOUR);
 	    $end       =   \DateTime::createFromFormat( 'H:i', self::MAX_HOUR);
-	    
+
 	    if ( $requested < $start || $requested > $end) {
 	        throw new OutOfBusinessHoursException( 'Not in allowed period.');
 	    }
-	    
+
 	    if ( $time->format( 'N') >= 6) {
 	        throw new OutOfBusinessHoursException( 'Weekedns not allowed.');
 	    }
 	}
-	
+
+    /**
+     * @param \DateTimeInterface $time
+     * @throws SlotNotAvailableException
+     * @throws OutOfBusinessHoursException
+     */
+    private function _checkSlotAvailability($time) {
+        $isSlotAvailable = $this->isSlotAvailable($time);
+        if (!$isSlotAvailable) {
+            throw new SlotNotAvailableException( 'The time slot [' . $time->format('Y-m-d H:s:i') . '] is already taken.');
+        }
+        if ($time->getTimestamp() <= time()) {
+            throw new SlotNotAvailableException( 'Time [' . $time->format('Y-m-d H:s:i') . '] in the past is not allowed.');
+        }
+    }
+
 	public function createAppointment( $email, $time, $payload = [])
 	{
+        $this->_checkSlotAvailability($time);
 	    $appointment_id    =   \Convo\Core\Util\StrUtil::uuidV4();
 	    $appointment       =   [
 	        'appointment_id' => $appointment_id,
@@ -105,19 +119,20 @@ class DummyAppointmentsContext extends AbstractBasicComponent implements IServic
 	        'timezone' => $time->getTimezone()->getName(),
 	        'payload' => $payload,
 	    ];
-	    
+
 	    $appointments      =   $this->_getAppointments();
 	    $appointments[]    =   $appointment;
 	    $this->_saveAppointments( $appointments);
-	    
+
 	    return $appointment_id;
 	}
-	
+
 	public function updateAppointment( $email, $appointmentId, $time, $payload = [])
 	{
+        $this->_checkSlotAvailability($time);
 	    $appointments      =   $this->_getAppointments();
 
-	    foreach ( $appointments as &$appointment) 
+	    foreach ( $appointments as &$appointment)
 	    {
 	        if ( $appointment['appointment_id'] != $appointmentId) {
 	            continue;
@@ -128,25 +143,25 @@ class DummyAppointmentsContext extends AbstractBasicComponent implements IServic
 	            $appointment['payload']  =   $payload;
 	        }
 	    }
-	    
+
 	    $this->_saveAppointments( $appointments);
 	}
-	
+
     public function cancelAppointment( $email, $appointmentId)
     {
         $appointments   =   $this->_getAppointments();
-        
+
         $appointments   =   array_filter( $appointments, function ( $appointment) use ( $appointmentId) {
             return $appointment['appointment_id'] != $appointmentId;
-        });        
-        
+        });
+
         $this->_saveAppointments( $appointments);
     }
 
     public function getAppointment( $email, $appointmentId)
     {
         $appointments      =   $this->_getAppointments();
-        
+
         $this->_logger->debug( 'Searching for appointment ['.$appointmentId.']');
         foreach ( $appointments as $appointment)
         {
@@ -156,7 +171,7 @@ class DummyAppointmentsContext extends AbstractBasicComponent implements IServic
                 return $appointment;
             }
         }
-        
+
         throw new DataItemNotFoundException( 'Could not find appointment ['.$appointmentId.']');
     }
 
@@ -164,58 +179,69 @@ class DummyAppointmentsContext extends AbstractBasicComponent implements IServic
     {
         $end        =   clone $startTime;
         $end        =   $end->modify( '+'.self::MAX_DAYS.' days' );
-        
+
         $this->_logger->info( 'Checking free slots from ['.$startTime->format( self::DATE_TIME_FORMAT).'] to ['.$end->format( self::DATE_TIME_FORMAT).']');
-        
+
         $interval   =   new \DateInterval( 'P1D');
         $daterange  =   new \DatePeriod( $startTime, $interval, $end);
-        
-        foreach ( $daterange as $day) 
+
+        foreach ( $daterange as $day)
         {
             if ( $day->format( 'N') >= 6) {
                 $this->_logger->info( 'Weekedns not allowed ['.$day->format( self::DATE_FORMAT).'].');
                 continue;
             }
-            
+
             $this->_logger->debug( 'Checking day ['.$day->format( self::DATE_TIME_FORMAT).']');
             /* @var \DateTime $day */
             $first      =   \DateTime::createFromFormat( 'H:i', self::MIN_HOUR);
             $last       =   \DateTime::createFromFormat( 'H:i', self::MAX_HOUR);
-            
+
             $this->_logger->debug( 'Checking day ['.$day->format( self::DATE_TIME_FORMAT).'] from ['.$day->format( self::TIME_FORMAT).'] to ['.$day->format( self::TIME_FORMAT).']');
-            
+
 //             PT30M
             $slots      =   new \DateInterval( 'PT'.self::DURATION_MINUTES.'M');
             $timerange  =   new \DatePeriod( $first, $slots, $last);
-            
-            foreach ( $timerange as $slot) 
+
+            $now = time();
+            foreach ( $timerange as $slot)
             {
                 /* @var \DateTime $slot */
                 // 'Y-m-d H:i:s';
-                $current    =   \DateTime::createFromFormat( 
-                    self::DATE_TIME_FORMAT, 
+                $current    =   \DateTime::createFromFormat(
+                    self::DATE_TIME_FORMAT,
                     $day->format( self::DATE_FORMAT).' '.$slot->format( self::TIME_FORMAT),
                     $startTime->getTimezone());
-                
+
                 $this->_logger->debug( 'Got current ['.$current->format( self::DATE_TIME_FORMAT).'] from slot ['.$slot->format( self::TIME_FORMAT).']');
-                
-                if ( $this->isSlotAvailable( $current)) {
-                    $this->_logger->debug( 'Returning match ['.$current->format( self::DATE_TIME_FORMAT).']');
-                    yield ['timestamp' => $current->getTimestamp()];                }
+
+                if ($current->getTimestamp() <= $now) {
+                    $this->_logger->info('Slot in past is not allowed.');
+                    continue;
+                }
+
+                try {
+                    if ($this->isSlotAvailable($current)) {
+                        $this->_logger->debug('Returning match [' . $current->format(self::DATE_TIME_FORMAT) . ']');
+                        yield ['timestamp' => $current->getTimestamp()];
+                    }
+                } catch (OutOfBusinessHoursException $e) {
+                    $this->_logger->notice($e->getMessage());
+                }
             }
         }
-        
+
 //         return new \ArrayIterator( [[
 //             'timestamp' => time() + 60 * 60 * 24,
 //         ], [
 //             'timestamp' => time() + 60 * 60 * 24 + 60 * 60 * 2,
-//         ]]);    
+//         ]]);
     }
 
-    public function loadAppointments( $email, $mode=self::LOAD_MODE_CURRENT, $count=self::DEFAULT_APPOINTMENTS_COUNT) 
+    public function loadAppointments( $email, $mode=self::LOAD_MODE_CURRENT, $count=self::DEFAULT_APPOINTMENTS_COUNT)
     {
         $appointments       =   $this->_getAppointments();
-        
+
         if ( $mode == self::LOAD_MODE_ALL) {
             return $appointments;
         }
@@ -237,7 +263,7 @@ class DummyAppointmentsContext extends AbstractBasicComponent implements IServic
         } else {
             throw new \Exception( 'Unexpected mode ['.$mode.']');
         }
-        
+
         return $filtered;
     }
 
@@ -245,7 +271,7 @@ class DummyAppointmentsContext extends AbstractBasicComponent implements IServic
     {
         return new \DateTimeZone( date_default_timezone_get());
     }
-    
+
     // DATA
     private function _getAppointments()
     {
@@ -258,15 +284,15 @@ class DummyAppointmentsContext extends AbstractBasicComponent implements IServic
         }
         return $this->_cachedAppointments;
     }
-    
+
     private function _saveAppointments( $appointments)
     {
         $params =   $this->getService()->getComponentParams( IServiceParamsScope::SCOPE_TYPE_USER, $this);
         $params->setServiceParam( 'appointments', $appointments);
         $this->_cachedAppointments  =   $appointments;
     }
-    
-    
+
+
     // UTIL
     public function __toString()
     {
